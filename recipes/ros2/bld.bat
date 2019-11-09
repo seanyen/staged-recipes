305af320ea7e31b6574c19ae5d8642a75c58525c
seanyen <seanyen@microsoft.com>
2019-11-08 20:15:34 -0800

    enable tracing.

--- recipes/ros2/bld.bat
+++ recipes/ros2/bld.bat
@@ -5,4 +5,8 @@ set CXX=cl.exe
 set ROS_DISTRO=dashing
 set ROS_PYTHON_VERSION=3
 
-colcon build --merge-install --cmake-args -G Ninja --install-base %LIBRARY_PREFIX%
+colcon build ^
+    --event-handlers console_cohesion+ ^
+    --merge-install ^
+    --cmake-args -G Ninja ^
+    --install-base %LIBRARY_PREFIX%
